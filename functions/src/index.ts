1cbe7d9fc2137609d8226efe85fb8bf0cdd3342d
Josh Gare <josh@jgapplications.com>
2025-08-10 09:53:29 +0100

    Setup hello world endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,11 +9,24 @@
 
 import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import express, { Request, Response } from "express";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// Initialize Express app
+const app = express();
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Middleware
+app.use(express.json());
+
+// Hello World endpoint
+app.get("/hello", (req: Request, res: Response) => {
+  logger.info("Hello endpoint called!", { structuredData: true });
+  res.json({ message: "Hello World from Firebase Functions!" });
+});
+
+// Health check endpoint
+app.get("/", (req: Request, res: Response) => {
+  res.json({ status: "API is running" });
+});
+
+// Export the Express app as a Firebase Function
+export const api = onRequest(app);
